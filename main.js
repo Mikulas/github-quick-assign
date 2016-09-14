563f251b6467677b256f30ca066797de7dfb7936
Mikulas <rullaf@gmail.com>
2016-09-14 10:05:31 +0200

    working assign

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.debug("quick assign");
+
+var assigneeForm = document.querySelector('.sidebar-assignee form');
+var avatars = document.querySelectorAll('.participation .participation-avatars a');
+var input = document.createElement('input');
+input.type = 'hidden';
+input.name = 'issue[user_assignee_ids][]';
+assigneeForm.appendChild(input);
+
+[].forEach.call(avatars, function(avatar) {
+	var src = avatar.querySelector('img').src;
+	var userId = /\/u\/(\d+)/.exec(src)[1];
+	console.debug(avatar, userId);
+
+	avatar.href = '#';
+	avatar.addEventListener('click', function(event) {
+		// remove default inputs from modal (only created if user clicked it)
+		assigneeForm.querySelector('.select-menu-modal-holder').remove();
+		input.value = userId;
+		assigneeForm.submit();
+
+		event.preventDefault();
+		event.stopPropagation();
+	}, false);
+});
